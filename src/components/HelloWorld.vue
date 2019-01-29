5509e2e8e8c32ffeedc4750e9e2f502b7c78e5c4
应若珩 <yingruoheng@higinet.com.cn>
2019-01-29 22:07:07 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>密码登录</h1>
+    <input placeholder="请输入用户名" type="text" name="userName"/>
+    <br><br>
+    <input placeholder="请输入密码" type="text" name="passWord"/>
+    <br><br>
+    <input type="submit" value="登录" />
+    <button @click="go">点我跳转</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      msg1: 'hello my app'
+    }
+  },
+
+  methods:{
+  	go(){
+  		this.$router.push('/Registered')
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Registered.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>我是Registered</h1>
+    <button @click="back">点我返回</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Registered',
+  data () {
+    return {
+    }
+  },
+
+  methods:{
+  	back(){
+  		this.$router.push('/')
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
